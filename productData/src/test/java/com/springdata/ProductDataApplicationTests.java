955553415fa2d641176c7e0adeff061285cf9c39
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-09 10:51:51 +0530

    Inserting Data in the Table

--- productData/src/test/java/com/springdata/ProductDataApplicationTests.java
+++ productData/src/test/java/com/springdata/ProductDataApplicationTests.java
@@ -2,15 +2,33 @@ package com.springdata;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springdata.product.entity.Product;
+import com.springdata.product.repository.ProductRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class ProductDataApplicationTests {
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testCreate()
+	{
+		Product product = new Product();
+		product.setId(1);
+		product.setName("IPhone");
+		product.setDesc("Awesome");
+		product.setPrice(1000d);
+		productRepository.save(product);
 	}
 
 }
