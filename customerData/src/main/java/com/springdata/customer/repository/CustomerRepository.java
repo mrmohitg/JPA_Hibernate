3d363cc178f75c097f138274bf3fbfc16099d842
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-10 16:58:37 +0530

    Customer Finder Method

--- customerData/src/main/java/com/springdata/customer/repository/CustomerRepository.java
+++ customerData/src/main/java/com/springdata/customer/repository/CustomerRepository.java
@@ -1,9 +1,17 @@
 package com.springdata.customer.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.springdata.customer.entity.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+	
+	List<Customer> findByNameAndEmail(String name, String email);
+	
+	List<Customer> findByEmailLike(String email);
+	
+	List<Customer> findByIdIn(List<Integer> id);
 
 }
--- customerData/src/test/java/com/springdata/CustomerDataApplicationTests.java
+++ customerData/src/test/java/com/springdata/CustomerDataApplicationTests.java
@@ -1,5 +1,8 @@
 package com.springdata;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +28,30 @@ class CustomerDataApplicationTests {
 	public void testCreateCustomer()
 	{
 		Customer customer = new Customer();
-		customer.setName("Mohit");
-		customer.setEmail("mohit.gmail.com");
+		customer.setName("Priya");
+		customer.setEmail("priya.yahoomail.com");
 		customerRepository.save(customer);
+	}
+	
+	@Test
+	public void testFindByNameAndEmail()
+	{
+		List<Customer> listOfCusomer = customerRepository.findByNameAndEmail("Mohit","mohit.gmail.com");
+		listOfCusomer.forEach(C -> System.out.println(C.getName()+" "+C.getEmail()));
+	}
+	
+	@Test
+	public void testFindByEmailLike()
+	{
+		List<Customer> listOfCusomer = customerRepository.findByEmailLike("%gmail%");
+		listOfCusomer.forEach(C -> System.out.println(C.getName()+" "+C.getEmail()));
+	}
+	
+	@Test
+	public void testFindByIdIn()
+	{
+		List<Customer> listOfCusomer = customerRepository.findByIdIn(Arrays.asList(1,2,3,4));
+		listOfCusomer.forEach(C -> System.out.println(C.getName()+" "+C.getEmail()));
 	}
 	
 }
