3c6c1901cd6c3291b82ba9a0e890f52cfb802565
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-10 18:20:58 +0530

    JPQL and Native SQL

---
+++ jpqlandnativesql/src/test/java/com/springdata/jpqlandnativesql/JpqlandnativesqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.jpqlandnativesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlandnativesqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
