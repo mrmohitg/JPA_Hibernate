d30c80578f2a8e1c8b33f2a0faacfc1571187d73
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-11 13:04:15 +0530

    All JPQL and Native Query Methods and Test Cases

---
+++ jpqlandnativesql/src/main/java/com/springdata/jpqlandnativesql/repository/StudentRepository.java
@@ -0,0 +1,30 @@
+package com.springdata.jpqlandnativesql.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.springdata.jpqlandnativesql.entity.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+	
+	@Query("from Student")
+	List<Student> findAllStudents();
+	
+	@Query("select st.firstName, st.lastName from Student st")
+	List<Object[]> findAllStudentsPartialData();
+	
+	@Query("from Student where firstName =:firstName")
+	List<Student> findAllStudentsByFirstName(@Param("firstName") String firstName);
+	
+	@Query("from Student where score > :minScore and score < :maxScore")
+	List<Student> findAllStudentsByScoresInBetween(@Param("minScore") int minScore, @Param("maxScore") int maxScore);
+	
+	@Modifying
+	@Query("delete from Student where firstName = :fName")
+	void deleteStudentByFirstName(@Param("fName") String firstName);
+
+}
--- jpqlandnativesql/src/test/java/com/springdata/jpqlandnativesql/JpqlandnativesqlApplicationTests.java
+++ jpqlandnativesql/src/test/java/com/springdata/jpqlandnativesql/JpqlandnativesqlApplicationTests.java
@@ -1,13 +1,88 @@
 package com.springdata.jpqlandnativesql;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springdata.jpqlandnativesql.entity.Student;
+import com.springdata.jpqlandnativesql.repository.StudentRepository;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class JpqlandnativesqlApplicationTests {
+	
+	@Autowired
+	private StudentRepository studentRepository;
 
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	void testStudentCreate() {
+		
+		Student student = new Student();
+		student.setFirstName("Nikhil");
+		student.setLastName("Gupta");
+		student.setScore(75);
+		studentRepository.save(student);
+		
+		Student student1 = new Student();
+		student1.setFirstName("Swati");
+		student1.setLastName("Gupta");
+		student1.setScore(66);
+		studentRepository.save(student1);
+		
+		Student student11 = new Student();
+		student11.setFirstName("Stuti");
+		student11.setLastName("Gupta");
+		student11.setScore(33);
+		studentRepository.save(student11);
+	}
+	
+	@Test
+	public void testFindAllStudent()
+	{
+		System.out.println(studentRepository.findAllStudents());
+	}
+	
+	@Test
+	public void testFindAllStudentsPartialData()
+	{
+		List<Object[]> findAllStudentsPartialData = studentRepository.findAllStudentsPartialData();
+		for (Object[] objects : findAllStudentsPartialData) {
+			System.out.println(objects[0]);
+			System.out.println(objects[1]);
+		}
+	}
+	
+	@Test
+	public void testFindAllStudentsByFirstName()
+	{
+		System.out.println(studentRepository.findAllStudentsByFirstName("Mohit"));
+	}
+	
+	@Test
+	public void testFindAllStudentsByScoresInBetween()
+	{
+		System.out.println(studentRepository.findAllStudentsByScoresInBetween(70,80));
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(false)
+	public void testDeleteStudentByFirstName()
+	{
+		studentRepository.deleteStudentByFirstName("Vipul");
+	}
+	
+	
+	
 }
