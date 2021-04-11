e41f831a1198621cf0d28565ad88531f76f52601
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-11 13:28:42 +0530

    JPQL and Native Query implementation

--- customerData/src/main/java/com/springdata/customer/repository/CustomerRepository.java
+++ customerData/src/main/java/com/springdata/customer/repository/CustomerRepository.java
@@ -2,7 +2,10 @@ package com.springdata.customer.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.springdata.customer.entity.Customer;
 
@@ -13,5 +16,9 @@ public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 	List<Customer> findByEmailLike(String email);
 	
 	List<Customer> findByIdIn(List<Integer> id);
+	
+	@Modifying
+	@Query("update Customer set email = :email where id = :id")
+	Integer updateCustomerEmail(@Param("id") int id,@Param("email") String email); 
 
 }
--- customerData/src/test/java/com/springdata/CustomerDataApplicationTests.java
+++ customerData/src/test/java/com/springdata/CustomerDataApplicationTests.java
@@ -3,10 +3,13 @@ package com.springdata;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.springdata.customer.entity.Customer;
@@ -52,6 +55,18 @@ class CustomerDataApplicationTests {
 	{
 		List<Customer> listOfCusomer = customerRepository.findByIdIn(Arrays.asList(1,2,3,4));
 		listOfCusomer.forEach(C -> System.out.println(C.getName()+" "+C.getEmail()));
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(false)
+	public void testUpdateCustomerEmail()
+	{
+		Integer cusomer = customerRepository.updateCustomerEmail(5,"priya.hotmail.com");
+		if(cusomer.intValue()>0)
+			System.out.println("Record updated sucessfully.");
+		else
+			System.out.println("Record updation failed.");
 	}
 	
 }
