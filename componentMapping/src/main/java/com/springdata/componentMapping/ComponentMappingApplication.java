1f39d3e298f5f889b013cd4206dfb13bcbcb0890
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-11 19:49:48 +0530

    Component Mapping Project

---
+++ componentMapping/src/main/java/com/springdata/componentMapping/ComponentMappingApplication.java
@@ -0,0 +1,13 @@
+package com.springdata.componentMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentMappingApplication.class, args);
+	}
+
+}
---
+++ componentMapping/src/test/java/com/springdata/componentMapping/ComponentMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.componentMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
