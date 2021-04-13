20d3df89b70263a774d569dde7d2cfb8be51f135
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-13 10:56:36 +0530

    CascadeType.ALL

--- associations/src/test/java/com/springdata/associations/AssociationsApplicationTests.java
+++ associations/src/test/java/com/springdata/associations/AssociationsApplicationTests.java
@@ -1,13 +1,50 @@
 package com.springdata.associations;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.HashSet;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springdata.associations.onetomany.entity.Actor;
+import com.springdata.associations.onetomany.entity.PhoneNumber;
+import com.springdata.associations.onetomany.repository.ActorRepository;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AssociationsApplicationTests {
+	
+	@Autowired
+	private ActorRepository actorRepository;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testCreateActor() {
+		Actor actor = new Actor();
+		actor.setName("Shahrukh Khan");
+		HashSet<PhoneNumber> numbers = new HashSet<PhoneNumber>();
+		PhoneNumber phoneNumber = new PhoneNumber();
+		phoneNumber.setNumber("9910374148");
+		phoneNumber.setType("Mobile");
+		phoneNumber.setActor(actor);
+		
+		numbers.add(phoneNumber);
+		
+		PhoneNumber phoneNumber1 = new PhoneNumber();
+		phoneNumber1.setNumber("9958878382");
+		phoneNumber1.setType("Mobile");
+		phoneNumber1.setActor(actor);
+		numbers.add(phoneNumber1);
+		
+		
+		actor.setNumbers(numbers);
+		
+		actorRepository.save(actor);
 	}
 
 }
