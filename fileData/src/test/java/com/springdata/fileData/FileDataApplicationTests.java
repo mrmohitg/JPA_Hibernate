7194d414d8dcafa1ba18a445d9a379a2db09eadc
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-17 12:57:43 +0530

    Storing Files in Database Project

---
+++ fileData/src/test/java/com/springdata/fileData/FileDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.fileData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
