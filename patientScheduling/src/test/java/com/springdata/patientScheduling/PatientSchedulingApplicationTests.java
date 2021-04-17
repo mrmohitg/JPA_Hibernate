519e9cc09bfd18ade289f326082955ae0dae70d2
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-17 16:33:30 +0530

    E2E JPA Project for PatientScheduling

---
+++ patientScheduling/src/test/java/com/springdata/patientScheduling/PatientSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.patientScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
