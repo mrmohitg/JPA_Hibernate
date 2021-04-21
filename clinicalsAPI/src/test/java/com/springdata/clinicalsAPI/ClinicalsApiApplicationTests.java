2434dd5ffba3cd97842aca2788c3f658697084f6
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-21 16:14:14 +0530

    Clinical API Project

---
+++ clinicalsAPI/src/test/java/com/springdata/clinicalsAPI/ClinicalsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.clinicalsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
