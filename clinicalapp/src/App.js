73b095065050f762a90884e7aed16a78013169bd
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-24 16:15:02 +0530

    React Front End Application

--- clinicalapp/src/App.js
+++ clinicalapp/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Switch} from 'react-router-dom';
+import CollectClinicals from './components/CollectClinicals';
+import AddPatient from './components/AddPatient';
+import ChartGenerator from './components/ChartGenerator';
+import AnalyzeData from './components/AnalyzeData';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/patientDetails/:patientId" component={CollectClinicals}/>
+        <Route exact path="/addPatient" component={AddPatient}/>
+        <Route exact path="/analyze/:patientId" component={AnalyzeData}/>
+        <Route exact path="/chart/:componentName/:patientId" component={ChartGenerator}/>
+      </Switch>
     </div>
   );
 }
